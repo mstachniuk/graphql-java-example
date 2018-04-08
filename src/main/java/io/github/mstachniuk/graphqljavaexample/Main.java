e89c8d7f0396a9740d20a8f7b22362a4bd87748a
Marcin Stachniuk <mstachniuk@gmail.com>
2018-04-08 11:23:35 +0200

    Add simple Customer endpoint

--- src/main/java/io/github/mstachniuk/graphqljavaexample/Main.java
+++ src/main/java/io/github/mstachniuk/graphqljavaexample/Main.java
@@ -1,11 +1,15 @@
 package io.github.mstachniuk.graphqljavaexample;
 
-import graphql.Scalars;
-import graphql.schema.GraphQLObjectType;
-import graphql.schema.GraphQLSchema;
+import graphql.schema.*;
+import graphql.schema.idl.RuntimeWiring;
+import graphql.schema.idl.SchemaGenerator;
+import graphql.schema.idl.SchemaParser;
+import graphql.schema.idl.TypeDefinitionRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.io.File;
 
 @SpringBootApplication
 public class Main {
@@ -16,15 +20,45 @@ public class Main {
 
     @Bean
     public GraphQLSchema schema() {
-        return GraphQLSchema.newSchema()
-                .query(GraphQLObjectType.newObject()
-                        .name("query")
-                        .field(field -> field
-                                .name("test")
-                                .type(Scalars.GraphQLString)
-                                .dataFetcher(environment -> "response")
-                        )
-                        .build())
+        SchemaParser schemaParser = new SchemaParser();
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource("graphql/model.graphqls").getFile());
+        TypeDefinitionRegistry registry = schemaParser.parse(file);
+        SchemaGenerator schemaGenerator = new SchemaGenerator();
+        RuntimeWiring runtimeWiring = RuntimeWiring.newRuntimeWiring()
+                .type("Query", builder ->
+                        builder.dataFetcher("customers", new CustomerDataFetcher("customers")))
                 .build();
+        GraphQLSchema graphQLSchema = schemaGenerator.makeExecutableSchema(registry, runtimeWiring);
+        return graphQLSchema;
+    }
+
+    public Customer getCustomerById() {
+        return new Customer("123", "name", "a@b.com");
+    }
+}
+
+class CustomerDataFetcher extends FieldDataFetcher {
+
+    public CustomerDataFetcher(String fieldName) {
+        super(fieldName);
+    }
+
+    @Override
+    public Object get(DataFetchingEnvironment environment) {
+        String id = environment.getArgument("id");
+        return new Customer(id, "name", "a@b.com");
+    }
+}
+
+class Customer {
+    private String id;
+    private String name;
+    private String email;
+
+    public Customer(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
     }
 }
